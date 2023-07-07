3458440647a2546169ad0a334b472a78d60c248d
the-ppz <frank1995alvcr@gmail.com>
2023-07-06 23:40:08 -0500

    Commit Frank

--- server/src/database/database.js
+++ server/src/database/database.js
@@ -2,7 +2,9 @@ import Sequelize from "sequelize";
 
 // conexion a la base de datos
 
-export const sequelize = new Sequelize("recetasdb", "postgres", "backend", {
-  host: "localhost",
+export const sequelize = new Sequelize("railway", "postgres", "UXxImbxGDcoevxmloyKQ", {
+  host: "containers-us-west-203.railway.app",
+  logging: false,
   dialect: "postgres",
+  port: 7939,
 });
